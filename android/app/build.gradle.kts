ae97fe1e93ee0c513ee70bb350f2f18762654107
Anandhu <connect.anandhukannan@gmail.com>
2026-07-24 17:16:46 +0530

    chore: revert compileSdkVersion to flutter.compileSdkVersion and remove redundant build configuration blocks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.lumo.customer.customer_app"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
