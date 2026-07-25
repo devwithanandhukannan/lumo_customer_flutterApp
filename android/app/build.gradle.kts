16dbe3c8fdac8da49b7913e0ac1f8f7ec4718f52
Anandhu <connect.anandhukannan@gmail.com>
2026-07-25 14:41:05 +0530

    build: update Android compileSdkVersion to 36 in project and app build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.lumo.customer.customer_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
